115e51228988da79faceba5f6aa6593d4a2d1e69
enricava <enricava@ucm.es>
2023-08-21 14:21:17 +0200

    Added copy command

--- src/lib.rs
+++ src/lib.rs
@@ -15,18 +15,20 @@ pub struct Config {
 
 impl Config {
     /// Arguments in config must be one of:
-    /// `create`, `list`, `add` `item`.
+    /// `create`, `list`, `add` `item`, `copy path`.
     pub fn new(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 2 {
-            return Err("not enough arguments. Usage: list, new, add [name]");
+            return Err("not enough arguments. Usage: list, new, add [name], copy [path]");
         }
 
         let cmd = args[1].clone();
 
-        let args = if cmd == "add" && args.len() < 3{
-            return Err("not enough arguments. Usage: list, new, add [name]");
+        let args = if (cmd == "add" || cmd == "copy") && args.len() < 3{
+            return Err("not enough arguments. Usage: list, new, add [name], copy [path]");
         } else if cmd == "add" {
             Some(args[2..].join(" ").clone())
+        } else if cmd == "copy"{
+            Some(args[2].clone())
         } else {
             None
         };
@@ -57,9 +59,23 @@ fn add_todolist(filepath: &PathBuf, item: String) -> Result<(), Box<dyn Error>> 
     Ok(())
 }
 
+fn copy_todolist(filepath: &PathBuf, otherpath: String) -> Result<(), Box<dyn Error>> {
+    let mut file = OpenOptions::new()
+        .write(true)
+        .append(true)
+        .open(filepath)?;
+
+    let copy_contents = fs::read_to_string(otherpath)?;
+
+    write!(file, "{}", copy_contents)?;
+
+    Ok(())
+}
+
 /// * `create` must create a new todo list
 /// * `list` must list current todo list
 /// * `add` `item` must insert a new todo item in the list
+/// * `copy` `otherpath` must copy a file into the list
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 
     let filepath: PathBuf = home_dir().unwrap_or_else(|| {
@@ -77,6 +93,9 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
         "add" => {
             add_todolist(&filepath, config.args.unwrap())
         }
-        _ => Err("unrecognized command.")?
+        "copy" => {
+            copy_todolist(&filepath, config.args.unwrap())
+        }
+        _ => Err("unrecognized command. Usage: list, new, add [name], copy [path]")?
     }
 }
